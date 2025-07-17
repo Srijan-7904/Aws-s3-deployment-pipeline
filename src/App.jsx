d078fd763a2063790744de4495f74393d1a5536b
Srijan Jaiswal <jaiswalsrijan79@gmail.com>
2025-07-18 00:25:47 +0530

    files is added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>CI/CD Pipeline Test</h1>
+      <p>This app is deployed using GitHub Actions and AWS S3.</p>
+    </div>
+  )
+}
+
+export default App
